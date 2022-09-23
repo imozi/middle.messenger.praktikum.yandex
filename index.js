bd63c9ed09337f36636f3322742fd005d1d566aa
Oleg Zaytsev <zaytsevoi.90@gmail.com>
2022-09-23 13:31:44 +1200

    fix: express server variables port and host

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 5500;
+const HOST = process.env.HOST || 'localhost';
+const url = `http://${process.env.HOST}:${process.env.PORT}`;
+
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 
-app.listen(
-  { port: process.env.PORT || 5500, host: process.env.HOST || 'localhost' },
-  () => {
-    const url = `http://${process.env.HOST || 'localhost'}:${
-      process.env.PORT || 3000
-    }`;
-
-    console.log(`🚀 Server ready at ${url}`);
-    open(url);
-  },
-);
+app.listen({ port: PORT, host: HOST }, () => {
+  // eslint-disable-next-line no-console
+  console.log(`🚀 Server ready at ${url}`);
+  open(url);
+});
