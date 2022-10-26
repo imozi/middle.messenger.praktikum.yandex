a5fbe9b5a217161fb462d1eff16bb233209b52aa
Oleg Zaytsev <zaytsevoi.90@gmail.com>
2022-10-26 19:45:43 +1200

    fix: moved  partial Profile

--- src/partials/index.ts
+++ src/partials/index.ts
@@ -1,5 +1,5 @@
 import Templater from 'core/Templater';
-import { Profile } from 'partials/Profile';
+import { Profile } from './Profile';
 
 const partials = [Profile];
 partials.forEach((partial) => Templater.regComponents(partial));
